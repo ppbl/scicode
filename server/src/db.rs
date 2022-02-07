549d99b339229335a80e167a48ca91959ac8ca09
Phosphophylliteeeeee <Phosphophylliteeeeee@outlook.com>
2022-02-07 17:19:20 +0800

    feat(server): add connection pool

--- server/src/db.rs
+++ server/src/db.rs
@@ -1,26 +1,56 @@
 use crate::models::*;
 use crate::schema::*;
-use diesel::pg::PgConnection;
 use diesel::prelude::*;
+use diesel::{
+    pg::PgConnection,
+    r2d2::{ConnectionManager, Pool, PooledConnection},
+};
 use dotenv::dotenv;
+use lazy_static::lazy_static;
 use std::env;
 
-pub fn establish_connection() -> PgConnection {
-    dotenv().ok();
+type PgPool = Pool<ConnectionManager<PgConnection>>;
 
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    PgConnection::establish(&database_url).expect(&format!("Error connecting to {}", database_url))
+pub struct ConnectionPool {
+    pub connection_pool: PgPool,
 }
+
+// pub fn establish_connection() -> PgConnection {
+//     dotenv().ok();
+//     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+//     PgConnection::establish(&database_url).expect(&format!("Error connecting to {}", database_url))
+// }
 
 pub fn create_post<'a>(conn: &PgConnection, title: &'a str, body: &'a str) -> Post {
     let new_post = NewPost {
-        title: title,
-        body: body,
-        published: &true
+        title,
+        body,
+        published: &true,
     };
 
     diesel::insert_into(posts::table)
         .values(&new_post)
         .get_result(conn)
         .expect("Error saving new post")
+}
+
+lazy_static! {
+    pub static ref CONN_POOL: ConnectionPool = {
+        dotenv().ok();
+        let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+        ConnectionPool {
+            connection_pool: r2d2::Pool::builder()
+                .max_size(15)
+                .build(ConnectionManager::new(&database_url))
+                .expect(&format!("Error connecting to {}", database_url)),
+        }
+    };
+}
+
+pub fn can_connect() -> bool {
+    self::CONN_POOL.connection_pool.get().is_ok()
+}
+
+pub fn get_connection() -> PooledConnection<ConnectionManager<PgConnection>> {
+    self::CONN_POOL.connection_pool.get().unwrap()
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -14,14 +14,18 @@ use models::*;
 #[get("/posts")]
 async fn posts() -> impl Responder {
     use schema::posts::dsl::*;
-    let connection = db::establish_connection();
-    let results = posts
-        .filter(published.eq(true))
-        .order(id.desc())
-        .limit(20)
-        .load::<Post>(&connection)
-        .expect("Error loading posts");
-    HttpResponse::Ok().json(results)
+    if db::can_connect() {
+        let connection = db::get_connection();
+        let results = posts
+            .filter(published.eq(true))
+            .order(id.desc())
+            .limit(20)
+            .load::<Post>(&connection)
+            .expect("Error loading posts");
+        HttpResponse::Ok().json(results)
+    } else {
+        HttpResponse::Ok().body("cannot connect to db")
+    }
 }
 
 #[derive(Deserialize)]
@@ -31,12 +35,16 @@ struct PostQuery {
 #[get("/post")]
 async fn post(query: web::Query<PostQuery>) -> impl Responder {
     use schema::posts::dsl::*;
-    let connection = db::establish_connection();
-    let results = posts
-        .find(query.id)
-        .get_result::<Post>(&connection)
-        .expect("Error loading posts");
-    HttpResponse::Ok().json(results)
+    if db::can_connect() {
+        let connection = db::get_connection();
+        let results = posts
+            .find(query.id)
+            .get_result::<Post>(&connection)
+            .expect("Error loading posts");
+        HttpResponse::Ok().json(results)
+    } else {
+        HttpResponse::Ok().body("cannot connect to db")
+    }
 }
 
 #[derive(Deserialize)]
@@ -47,10 +55,14 @@ struct Body {
 
 #[post("/create_post")]
 async fn create_post(req_body: web::Json<Body>) -> impl Responder {
-    let connection = db::establish_connection();
-    println!("{}", req_body.title);
-    db::create_post(&connection, &req_body.title, &req_body.body);
-    HttpResponse::Ok().body("success")
+    if db::can_connect() {
+        let connection = db::get_connection();
+        println!("{}", req_body.title);
+        db::create_post(&connection, &req_body.title, &req_body.body);
+        HttpResponse::Ok().body("success")
+    } else {
+        HttpResponse::Ok().body("cannot connect to db")
+    }
 }
 
 #[actix_web::main]
